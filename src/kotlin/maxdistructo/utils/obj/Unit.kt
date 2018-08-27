2d2a354b13a6dc96897dfc4b56f882664641ad67
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-08-27 14:13:49 -0500

    Create Unit.kt

---
+++ src/kotlin/maxdistructo/utils/obj/Unit.kt
@@ -0,0 +1,12 @@
+package maxdistructo.utils.obj
+
+class IntUnit : Any{
+  var value : Int
+  var unit : String
+  
+  constructor(valueIn : Int, unitIn : String){
+    value = valueIn
+    unit = unitIn
+  }
+  
+}
