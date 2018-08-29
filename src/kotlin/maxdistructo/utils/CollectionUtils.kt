63dce217758d8ad4405a667fd514d1dfcb2e1817
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-08-29 11:51:02 -0500

    Update CollectionUtils.kt

--- src/kotlin/maxdistructo/utils/CollectionUtils.kt
+++ src/kotlin/maxdistructo/utils/CollectionUtils.kt
@@ -2,7 +2,7 @@ package maxdistructo.utils
 
 object CollectionUtils{
 
-  fun jsonArrayToList(jsonArray: JSONArray) : List<Any>{
+    fun jsonArrayToList(jsonArray: JSONArray) : List<Any>{
         var i = 0
         var outList = listOf<Any>()
         while(i < jsonArray.count()){
@@ -11,21 +11,34 @@ object CollectionUtils{
         }
         return outList
     }
-    fun arrayToList(array : Array<String>) : List<String> {
-        var output : List<String> = listOf()
-        for(value in array){
-            output += value
-        }
-        return output
+    fun jsonArrayToArray(jsonArray : JSONArray) : Array<Any>{ //JSONArray returns only objects and because this code does not know what the values are specifically, it returns an array with Any/Object values
+      var output = arrayOf<Any>(jsonArray.length)
+      for(value in jsonArray){
+        output.add(value)
+      }
+      return output
     }
-    fun listToArray(list : List<String>) : Array<String>{
-        val output : ArrayList<String> = arrayListOf()
-        for(value in list){
-            output.add(value)
-        }
-        return output.toTypedArray()
+    fun collectionToJsonArray(input : Collection<*>) : JSONArray{
+        return JSONArray(input)
     }
-    
-    
-
+    fun arrayToList(array : Array<String>) : List<String> { //Legacy & Convenience
+        return collectionToList(array)
+    }
+    fun listToArray(list : List<String>) : Array<String>{ //Legacy & Convenience
+        return collectionToArray(list)
+    }
+    fun collectionToArray(input : Collection<T>) : Array<T>{
+      var output : Array<T> = arrayOf<T>(input.size)
+      for (value in input){
+        output.add(value)
+      }
+      return output
+    }
+    fun collectionToList(input : Collection<T>) : List<T>{
+      var output : List<T> = listOf<T>()
+      for(value in input){
+        output += value
+      }
+      return output
+    }
 }
