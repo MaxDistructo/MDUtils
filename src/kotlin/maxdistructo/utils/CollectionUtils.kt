3978edd3856595ca09fc15a8a27ae5bafe8703bc
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-08-29 10:13:39 -0500

    Create CollectionUtils.kt

---
+++ src/kotlin/maxdistructo/utils/CollectionUtils.kt
@@ -0,0 +1,31 @@
+package maxdistructo.utils
+
+object CollectionUtils{
+
+  fun jsonArrayToList(jsonArray: JSONArray) : List<Any>{
+        var i = 0
+        var outList = listOf<Any>()
+        while(i < jsonArray.count()){
+            outList += jsonArray[i]
+            i++
+        }
+        return outList
+    }
+    fun arrayToList(array : Array<String>) : List<String> {
+        var output : List<String> = listOf()
+        for(value in array){
+            output += value
+        }
+        return output
+    }
+    fun listToArray(list : List<String>) : Array<String>{
+        val output : ArrayList<String> = arrayListOf()
+        for(value in list){
+            output.add(value)
+        }
+        return output.toTypedArray()
+    }
+    
+    
+
+}
